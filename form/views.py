e8a7194f58b13f2efd5d4b64e4094d42e7eff93b
Vika Putri Ariyanti <vikaputri909@gmail.com>
2023-10-02 05:53:45 +0700

    update

--- form/views.py
+++ form/views.py
@@ -18,7 +18,7 @@ def data_list(request):
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_201_CREATED)
         return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(['GET', 'DELETE'])
 def data_detail(request, pk):
     try:
         form = Form.objects.get(pk=pk)
@@ -28,13 +28,6 @@ def data_detail(request, pk):
     if request.method == 'GET':
         serializer = Formserializer(form)
         return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = Formserializer(form, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         form.delete()
